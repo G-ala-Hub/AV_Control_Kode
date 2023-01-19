c35aaa8cc5dabfc21044d5968d5b07b725b5f9ef
G-ala-Hub <91773328+G-ala-Hub@users.noreply.github.com>
2023-01-19 21:57:06 +1100

    Updated nav
    After our chat with AK we decided to remove our mission section.

---
+++ contact.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+if (isset)($_POST)['submit'])) {
+
+    $cf-name = $_POST['cf-name'];
+    $cf-email = $_POST['cf-email'];
+    $cf-subject = $_POST['cf-subject'];
+    $cf-message = $_POST['cf-message']; 
+
+    $mailTo = "geraldwala101@gmail.com";
+    $headers = "From: ".$cf-email;
+    $txt = "You have recived a email form".$cf-name.".\n\n".$cf-message;
+
+
+    mail($mailTo, $headers, $txt, $headers);
+    header("Location: index.php?mailsent");
+
+}
